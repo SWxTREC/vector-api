d9dd9a41ee6fabdac3591841cf9abac6051bc1a7
Greg Lucas <greg.lucas@lasp.colorado.edu>
2020-03-06 13:28:22 -0700

    Initializing an api/backend specific repository.

---
+++ README.md
@@ -0,0 +1,130 @@
+# API Definition
+
+Version 0.1
+
+Overview of the input/output requirements for VECTOR.
+
+## Endpoints
+
+- /api/image : POST
+
+This endpoint accepts a VRML `.wrl` file for upload and returns a `.png` image of the file.
+
+- /api/singlepoint : POST
+
+This endpoint is to submit single-point calculation parameters to.
+The backend will then parse the payload and run VECTOR with the proper inputs to get the return values.
+The return from the VECTOR code will then be put in the response payload to send back to the frontend.
+
+## Inputs
+
+objectType: string
+
+`cylinder, sphere, plate, plate model`
+
+![Objects](https://raw.githubusercontent.com/SWxTREC/vector-code/master/docs/vector_objects.png)
+
+diameter: float
+
+`The diameter of the object [m]`
+
+length: float
+
+`The length of the object [m]`
+
+area: float
+
+`The area of the object [m^2]`
+
+pitch: float
+
+`Pitch angle of the object [deg]`
+
+sideslip: float
+
+`Sideslip angle of the object [deg]`
+
+temperature: float
+
+`Ambient temperature of the atmosphere [K]`
+
+speed: float
+
+`Speed of the object [m/s]`
+
+composition: dictionary
+
+```text
+Number density composition of atmospheric constituents [/m3]
+
+
+O: float
+
+O2: float
+
+N2: float
+
+He: float
+
+H: float
+```
+
+accommodationModel: string
+
+`SESAM, Goodman, Fixed`
+
+surfaceMass: float
+
+`Surface mass of the object [amu]`
+
+energyAccomodation: float
+
+`Energy accomodation value for the object [-]`
+
+### Example Input Payload
+
+```json
+{
+"objectType": "cylinder",
+"diameter": 1.25,
+"length": 2.5,
+"area": 0,
+"pitch": 30,
+"sideslip": 0,
+"temperature": 800,
+"speed": 7750,
+"composition": {"O": 1e11,
+                "O2": 1e6,
+                "N2": 1e6,
+                "He": 1e6,
+                "H": 1e4},
+"accommodationModel": "SESAM",
+"energyAccomodation": 0,
+"surfaceMass": 0}
+}
+```
+
+## Outputs
+
+dragCoefficient: float
+
+`The coefficient of drag of the object [-]`
+
+projectedArea: float
+
+`The projected area of the object [m2]`
+
+forceCoefficent: float
+
+`The force coefficient of the object [m2]`
+
+### Example Output Payload
+
+```json
+{
+"dragCoefficient": 2.8,
+"projectedArea": 3,
+"forceCoefficient": 1.2,
+"energyAccommodation": 0.930
+}
+```
---
+++ application.py
@@ -0,0 +1,106 @@
+from tempfile import NamedTemporaryFile
+from flask import Flask, abort, jsonify, request, send_file
+from flask_cors import CORS
+
+import matlab.engine
+eng = matlab.engine.start_matlab()
+# path on EB ec2
+eng.cd('/opt/python/current/app/vector-code/CD_CODE')
+# Testing on local ec2
+# eng.cd('/home/ec2-user/vector-api/vector-code/CD_CODE')
+
+application = Flask(__name__)
+CORS(application)
+# Limit the content upload size to 16 MB
+application.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+ALLOWED_EXTENSIONS = {'wrl'}
+
+
+def call_matlab(d):
+    """Call the matlab main routine with the input dictionary `d`."""
+
+    # Used for MATLAB input file parsing
+    OBJ_TYPE_DICT = {'sphere': 1, 'plate': 2,
+                     'cylinder': 3, 'geometry_file': 4}
+    ACCOM_MODEL_DICT = {'SESAM': -1, 'Fixed': 0, 'Goodman': 2}
+    # Matlab doesn't like ints... So change them to floats
+    for key in d:
+        if isinstance(d[key], int):
+            d[key] = float(d[key])
+
+    composition = d["composition"]
+    for key in composition:
+        # MATLAB wants floats...
+        composition[key] = float(composition[key])
+
+    out = eng.MAIN(OBJ_TYPE_DICT[d["objectType"]], d["diameter"], d["length"],
+                   d["area"], d["pitch"], d["sideslip"], d["temperature"],
+                   d["speed"], composition["o"], composition["o2"],
+                   composition["n2"], composition["he"], composition["h"],
+                   ACCOM_MODEL_DICT[d["accommodationModel"]],
+                   d["energyAccommodation"], d["surfaceMass"], [[]], "",
+                   nargout=5)
+
+    # Make a dict for json return
+    d_return = {"dragCoefficient": out[1],
+                "projectedArea": out[2],
+                "forceCoefficient": out[3],
+                "energyAccommodation": out[4]}
+    return d_return
+
+
+def call_matlab_image(fname):
+    """Call the matlab image generation function with the input filename.
+
+    Returns
+    -------
+    filepath : str
+        Path to the created image.
+    """
+    # TODO: Still need to add the calls to the actual Matlab code.
+    return 'test.png'
+
+
+@application.route('/api/singlepoint', methods=['POST'])
+def vector_matlab():
+    if not request.json:
+        abort(400, "No json content in the request.")
+
+    output = call_matlab(request.json)
+    return jsonify(output)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@application.route('/api/image', methods=['POST'])
+def generate_image():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        abort(400, "No 'file' in the request.")
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        abort(400, "The filename  in the request was empty.")
+
+    if not allowed_file(file.filename):
+        abort(400, "Bad file extension, only '.wrl' filetypes are allowed")
+
+    print("GML:", file.filename)
+    if file and allowed_file(file.filename):
+        # Save the uploaded file to a temporary file and then pass
+        # that on to the matlab image generation code
+        with NamedTemporaryFile() as f:
+            file.save(f.name)
+            image_fname = call_matlab_image(f.name)
+            return send_file(image_fname, mimetype='image/png')
+
+    # If we made it here, there was an unknown error
+    abort(400, "Unknown error handling the uploaded image")
+
+
+if __name__ == '__main__':
+    application.run(debug=True)
