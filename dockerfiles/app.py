fda58b9d6f781afc053f21ec8bc0a5a0eb16698a
Greg Lucas <greg.m.lucas@gmail.com>
2023-05-08 16:44:48 -0600

    MNT: Add lots of logging
    Add extra traceback logging within the app to know where it is failing
    and get a full traceback from Python.

--- dockerfiles/app.py
+++ dockerfiles/app.py
@@ -3,22 +3,36 @@
 # Compile on Linux:
 # https://www.mathworks.com/help/compiler_sdk/python/compiler.build.pythonpackage.html
 # https://adam.isi.uu.nl/methods/dockerfile/example-matlab/
+import faulthandler
+import sys
+faulthandler.enable(file=sys.stdout, all_threads=True)
+import json
 import os
 import logging
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
+
 
 import boto3
 try:
     BUCKET = boto3.resource('s3').Bucket(os.environ['S3_BUCKET'])
+    logger.info("Using S3_BUCKET: %s", os.environ['S3_BUCKET'])
 except KeyError:
     # Local install
     logger.warning("S3_BUCKET environment variable not set. Using local install.")
     BUCKET = None
 
+logger.info("LD_LIBRARY_PATH: [%s]", os.environ.get("LD_LIBRARY_PATH", ""))
+logger.info("PYTHONPATH: [%s]", os.environ.get("PYTHONPATH", ""))
+logger.info("PATH: [%s]", os.environ.get("PATH", ""))
 
 def vector_handler(event, context):
-    data = event['body']  # json.loads(event['body'])
+    # Remote testing does need to string load the event
+    if BUCKET is not None:
+        data = json.loads(event['body'])
+    else:
+        # Local testing doesn't send the event as a JSON string
+        data = event['body']
     logger.info("Input JSON body: %s", data)
 
     # Matlab doesn't like ints... So change them to floats
@@ -62,16 +76,19 @@ def vector_handler(event, context):
 def run_matlab(data):
     """Runs the matlab VECTOR code"""
     logger.info("Running matlab with options: %s", data)
+    logger.info("Beginning imports from v5")
     import vector_main
+    logger.info("vector_main loaded")
     vector_main.initialize_runtime(['-nojvm', '-nodisplay'])
+    logger.info("vector_main initialized")
     # Import the matlab module only after you have imported 
     # MATLAB Compiler SDK generated Python modules.
     from matlab import double as mdouble
-
+    logger.info("matlab double imported")
     try:
         my_vector_main = vector_main.initialize()
     except Exception as e:
-        print(f'Error initializing Vector_main package\\n:{e}')
+        logger.error(f'Error initializing Vector_main package\\n:{e}')
         exit(1)
     logger.info("Initialized Matlab runtime")
 
@@ -99,6 +116,7 @@ def run_matlab(data):
 
     logger.info("Matlab returned: %s", out)
     if os.path.exists("geometry.png"):
+        logger.info("Uploading geometry.png to S3: %s", f"/data/vector/{data['userId']}.png")
         BUCKET.upload_file("geometry.png", f"/data/vector/{data['userId']}.png")
     # Make a dict for json return
     d_return = {"dragCoefficient": out[1],
